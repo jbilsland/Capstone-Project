0d5f3a265c8a09a438a4c63a5a5707d8d08ff729
Jack Bilsland <JackBilsland@creighton.edu>
2021-10-06 08:58:25 -0500

    init

---
+++ src/main/java/com/jack/bils/surveysitebackendapi1_0/SurveySiteBackendApi10Application.java
@@ -0,0 +1,13 @@
+package com.jack.bils.surveysitebackendapi1_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveySiteBackendApi10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveySiteBackendApi10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jack/bils/surveysitebackendapi1_0/SurveySiteBackendApi10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.bils.surveysitebackendapi1_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveySiteBackendApi10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
