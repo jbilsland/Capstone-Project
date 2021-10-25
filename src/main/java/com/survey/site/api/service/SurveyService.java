5c173ca46c43a3a5e9eea8ebe179a35c1160c80b
Jack Bilsland <JackBilsland@creighton.edu>
2021-10-25 12:15:18 -0500

    whitespace

--- src/main/java/com/survey/site/api/service/SurveyService.java
+++ src/main/java/com/survey/site/api/service/SurveyService.java
@@ -1,12 +1,16 @@
 package com.survey.site.api.service;
 
+import com.survey.site.api.mappers.SurveyMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SurveyService {
     private static final Logger LOGGER = LoggerFactory.getLogger(SurveyService.class);
+    @Autowired
+    private SurveyMapper mapper;
 
     public void handle(){}
 }
